a616f7eb808f428c2f8bc5d85a73edad7c3f97b3
Mila <turpeinenmila6@gmail.com>
2024-10-01 02:09:39 +0300

    Worked on level 6 and game bounds

---
+++ Parking Simulator/Assets/Scripts/Barriers.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class Barriers : MonoBehaviour
+{
+    [SerializeField] TMP_Text warning;
+    [SerializeField] TMP_Text timer;
+    Collider areaCollider;
+    [SerializeField] Transform player;
+    Coroutine restartLevel;
+    [SerializeField] int currentLevel;
+
+    void Start()
+    {
+        areaCollider = GetComponent<Collider>();
+
+  
+    }
+  
+        void Update()
+        {
+            if (areaCollider.bounds.Contains(player.transform.position))
+            {
+                warning.enabled = false;
+            timer.enabled = false;
+            if (restartLevel != null)
+            {
+                StopCoroutine(restartLevel);
+            
+                restartLevel = null;
+            }
+            }
+            else
+            {
+            warning.enabled = true;
+            if (restartLevel == null)
+            {
+             restartLevel = StartCoroutine(OutOfBoundsWarning(15));
+
+            }
+            }
+
+        }
+
+    IEnumerator OutOfBoundsWarning(float time)
+    {
+        timer.color = Color.white;
+        timer.enabled = true;
+
+        float currentTime = time;
+
+        while (currentTime > 0)
+        {
+
+            timer.text = currentTime.ToString("0");
+
+            yield return new WaitForSeconds(1.0f);
+
+            currentTime--;
+
+            if (currentTime < 6)
+            {
+                timer.color = Color.red;
+            }
+        }
+
+        SceneManager.LoadScene(currentLevel);
+
+    }
+}
